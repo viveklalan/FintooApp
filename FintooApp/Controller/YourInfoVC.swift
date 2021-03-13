a30df59d79af1e8a3923da860fbf22967d42442d
ms-vivek <vivek.kantilal@markelytics.com>
2021-03-13 20:27:27 +0530

    completed assignment functionality

---
+++ FintooApp/Controller/YourInfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  YourInfoVC.swift
+//  FintooApp
+//
+//  Created by Jai Mataji on 13/03/21.
+//
+
+import UIKit
+
+class YourInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
